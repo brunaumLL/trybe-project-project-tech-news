c725a0854466bb61e4af3fd2600e4abe28bd819e
brunaumLL <bruno99louzada@gmail.com>
2023-03-14 16:39:03 -0300

    requisito 03 feito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,8 +29,12 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
-
+    try:
+        selector = Selector(html_content)
+        next_page = selector.css(".next::attr(href)").get()
+        return next_page
+    except not next_page:
+        return None
 
 # Requisito 4
 def scrape_noticia(html_content):
