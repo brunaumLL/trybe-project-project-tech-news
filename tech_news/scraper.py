945d2cb88668a9dd21ba274f1f3ed192b1ef0712
brunaumLL <bruno99louzada@gmail.com>
2023-03-14 18:01:34 -0300

    requisito 04 feito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,9 +36,34 @@ def scrape_next_page_link(html_content):
     except not next_page:
         return None
 
+
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".meta-author a::text").get()
+    comments_text = selector.css(".post-comments .title-block::text").get()
+    if not comments_text:
+        comments_count = 0
+    else:
+        comments_count = comments_text.split(' ')[0]
+    summary = selector.css(".entry-content > p:first-of-type *::text").getall()
+    summary = "".join(summary).strip()
+    tags = selector.css(".post-tags a::text").getall()
+    category = selector.css("span.label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
