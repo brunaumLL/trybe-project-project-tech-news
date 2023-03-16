ebf04de422ad01610dc2bdd8dfec721f246089e0
brunaumLL <bruno99louzada@gmail.com>
2023-03-16 16:18:56 -0300

    requisito 10 feito

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
+from tech_news.database import get_collection
+import pymongo
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    db = get_collection()
+    order_news = db.find().sort([
+        ("comments_count", pymongo.DESCENDING),
+        ("title", pymongo.ASCENDING)]).limit(5)
+    news_list = []
+    for new in order_news:
+        news_list.append((new["title"], new["url"]))
+    return news_list
 
 
 # Requisito 11
